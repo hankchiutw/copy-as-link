4ee56dd97d0b92af82e8f9f488ddf135c3f757cb
hank <hankchiu.tw@gmail.com>
2024-10-29 23:11:57 +0800

    refactor(content): implement CopiedInfo

--- contents/CopyAsLinkMessage.tsx
+++ contents/CopyAsLinkMessage.tsx
@@ -1,52 +1,77 @@
-import { notification } from 'antd';
 import icon32 from 'data-base64:~assets/icon-32.png';
 import { useEffect, useState } from 'react';
 
 import { copyTextAsHtml } from '~features/copy-text-as-html';
 import { useSelectionListener } from '~features/useSelectionListener';
 
+const CopyAnchor = ({ onClick, position }) => {
+  return (
+    <span
+      onClick={onClick}
+      title="copy as link"
+      style={{
+        position: 'absolute',
+        top: position.y,
+        left: position.x,
+        width: '16px',
+        height: '16px',
+        cursor: 'pointer',
+      }}>
+      <img src={icon32} alt="copy as link" style={{ width: '100%' }} />
+    </span>
+  );
+};
+
+const CopiedInfo = ({ position }) => {
+  const [opacity, setOpacity] = useState(1);
+  useEffect(() => {
+    const fadeOut = setTimeout(() => {
+      setOpacity(0);
+    });
+
+    return () => clearTimeout(fadeOut);
+  }, []);
+
+  return (
+    <span
+      style={{
+        position: 'absolute',
+        top: position.y,
+        left: position.x,
+        lineHeight: '12px',
+        fontSize: '12px',
+        whiteSpace: 'nowrap',
+        backgroundColor: '#a3a3a3',
+        color: '#4d4d4d',
+        borderRadius: '8px',
+        padding: '2px 8px',
+        fontFamily: 'sans-serif',
+        transition: 'opacity 2s ease-in',
+        opacity,
+      }}>
+      copied as link!
+    </span>
+  );
+};
+
 const CopyAsLinkMessage = () => {
   const { position, selectedText } = useSelectionListener();
-  const [iconVisible, setIconVisible] = useState(true);
-  const [api, contextHolder] = notification.useNotification({
-    duration: 5,
-    placement: 'topLeft',
-  });
-
-  const shouldRenderIcon = position && iconVisible;
+  const [hintElement, setHintElement] = useState(null);
 
   const onClickIcon = async () => {
-    const CopiedAnchor = await copyTextAsHtml(selectedText);
-    setIconVisible(false);
-    api.info({
-      message: <>Copied {CopiedAnchor}</>,
-    });
+    await copyTextAsHtml(selectedText);
+    setHintElement(<CopiedInfo position={position}></CopiedInfo>);
   };
 
   useEffect(() => {
-    setIconVisible(true);
+    if (position) {
+      setHintElement(
+        <CopyAnchor onClick={onClickIcon} position={position}></CopyAnchor>,
+      );
+    }
   }, [position]);
 
-  return (
-    <>
-      {contextHolder}
-      {shouldRenderIcon && (
-        <span
-          onClick={onClickIcon}
-          title="copy as link"
-          style={{
-            position: 'absolute',
-            top: position.y,
-            left: position.x,
-            width: '16px',
-            height: '16px',
-            cursor: 'pointer',
-          }}>
-          <img src={icon32} alt="copy as link" style={{ width: '100%' }} />
-        </span>
-      )}
-    </>
-  );
+  return <>{position && hintElement}</>;
 };
 
 export default CopyAsLinkMessage;
